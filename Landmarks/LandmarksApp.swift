039f9428f975923972fea3dfbd4c7bfce4c3bbf3
Andrew Huang <vivatv.it0001@gmail.com>
2025-06-17 14:00:54 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by IT-MAC-02 on 2025/6/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
