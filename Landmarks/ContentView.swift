ed5dc4092338041cb41638a656c7f89422e3f96a
Andrew Huang <vivatv.it0001@gmail.com>
2025-06-17 17:29:18 +0800

    [Creating and combining views] Section6: Compose the detail view

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,19 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
+        VStack {
+            MapView()
+                .frame(height: 300) // 可以自定義view的frame
             
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+            CircleImage()
+                .offset(y: -130)        // 往上偏移130
+                .padding(.bottom, -130) // 與bottom的間隙減少130
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)             // 將modifier放在Stack上會適用於裡面的所有畫面
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
