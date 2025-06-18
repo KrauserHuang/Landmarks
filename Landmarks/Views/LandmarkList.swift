806e738b73f679b6758d47fde2193c4c8277fe55
Andrew Huang <vivatv.it0001@gmail.com>
2025-06-18 15:05:26 +0800

    Section3: Add a control to toggle the state
    1. Add ForEach for dynamic data type
    2. Add Toggle to switch filter landmarks action

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct LandmarkList: View {
     
-    @State private var showFavoritesOnly: Bool = true
+    @State private var showFavoritesOnly: Bool = false
     
     var filteredLandmarks: [Landmark] {
         landmarks.filter { landmark in
@@ -19,13 +19,20 @@ struct LandmarkList: View {
     
     var body: some View {
         NavigationSplitView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {                                          // List是給靜態(static)資料
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in    // ForEach是給動態(dynamic)資料
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
+            .animation(.default, value: filteredLandmarks)  // improve filtering animation
             .navigationTitle("Landmarks")
         } detail: {
             Text("Select a Landmark")
