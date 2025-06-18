677c8cfe5e94ac86473e5108faaae4c4c821889a
Andrew Huang <vivatv.it0001@gmail.com>
2025-06-18 15:49:36 +0800

    Section4: Use observation for storage
    1. Add ModelData class with @Observable macro then move global variable landmarks into ModelData

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 
-var landmarks: [Landmark] = load("landmarkData.json")
+// SwiftUI updates a view only when an observable property changes and the view’s body reads the property directly.
+// 被Observable macro的型別，可被SwiftUI觀察，該實體物件有任何變動都會讓view可以更新
+@Observable
+class ModelData {
+    var landmarks: [Landmark] = load("landmarkData.json")
+}
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
