2514f55dc9d79782be21507fb3c8be9d003e6d10
Andrew Huang <vivatv.it0001@gmail.com>
2025-06-18 13:44:54 +0800

    [Building lists and navigation] Section6: Set up navigation between list and detail
    1. Create LandmarkDetail and switch code from ContentView to LandmarkDetail
    2. Add navigation to LandmarkList
    3. Put LandmarkList to ContentView

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,47 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by IT-MAC-02 on 2025/6/18.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300) // 可以自定義view的frame
+            
+            CircleImage()
+                .offset(y: -130)        // 往上偏移130
+                .padding(.bottom, -130) // 與bottom的間隙減少130
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)             // 將modifier放在Stack上會適用於裡面的所有畫面
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
